b8e64e09cf7b085b0e9e8d6ff0775c43eb6267e2
undercore <undercore@localhost>
2012-10-16 20:38:54 +0000

    alphabetized admin sections, closes #588
    svn path=/plant/trunk/; revision=6513

--- core/controllers/AdminController.class.php
+++ core/controllers/AdminController.class.php
@@ -17,7 +17,7 @@
 	 * @license http://opensource.org/licenses/gpl-3.0.html GNU General Public License, version 3
 	 * @package plant_core
 	 * @subpackage controllers
-	 * @version 1.3
+	 * @version 1.4
 	 */
 	class AdminController extends Controller {
 		
@@ -41,11 +41,12 @@
 			if (!$this->user->isLoggedIn()) return false;
 			
 			// Set navigation used throughout admin pages
-			$adminNav = array();		
-			if ($this->user->is("admin")) $navPaths = Model::getAllSQL("path", "JOIN path AS parentpath ON parentpath.path = '" . $this->getToken() . "' AND parentpath.id = path.parent");
-			else $navPaths = Model::getAllSQL("path", "JOIN path AS parentpath ON parentpath.path = '" . $this->getToken() . "' AND parentpath.id = path.parent JOIN link_path_usergroup ON path.id = link_path_usergroup.path_id AND link_path_usergroup.usergroup_id = " . $this->user->getGroup()->getID());
+			$navQuery["joins"][] = "JOIN path AS parentpath ON parentpath.path = '" . $this->getToken() . "' AND parentpath.id = path.parent";
+			if (!$this->user->is("admin")) $navQuery["joins"][] = "JOIN link_path_usergroup ON path.id = link_path_usergroup.path_id AND link_path_usergroup.usergroup_id = " . $this->user->getGroup()->getID();
+			$navQuery["order"] = "path.title ASC";
 			
-			if ($navPaths) {
+			$adminNav = array();
+			if ($navPaths = Model::getAllSQL("path", $navQuery)) {
 				foreach($navPaths as $navPath) {
 					$adminNav[ucfirst($navPath->getPath())] = $this->getPath() . $navPath->getPath() . "/";
 				}

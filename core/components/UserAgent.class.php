cc4d998ba10cc1b87f1a0eddab9b64ae3b120c9b
undercore <undercore@localhost>
2012-10-26 01:10:24 +0000

    useragent update
    svn path=/plant/trunk/; revision=6564

--- core/components/UserAgent.class.php
+++ core/components/UserAgent.class.php
@@ -18,7 +18,7 @@
 	 * @license http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
 	 * @package plant_tools
 	 * @subpackage components
-	 * @version 1.3
+	 * @version 1.4
 	 */
 	class UserAgent {
 		
@@ -516,32 +516,32 @@
   			$this->rawAccept = isset($_SERVER["HTTP_ACCEPT"]) ? $_SERVER["HTTP_ACCEPT"] : false;
   			
   			// Set default mobile property
-  			$this->mobile = true;  	
+  			$this->mobile = true;
   			
   			// Check for device override
   			if (isset($_REQUEST["device"])) $deviceOverride = $_REQUEST["device"];
   			else if (Session::get("device")) $deviceOverride = Session::get("device");
   			else $deviceOverride = false;
-  			
+  			  			
   			// Unset UA string
   			if ($deviceOverride) $this->rawUA = false;
   			
   			// Detect user agent in use
 			switch(true) {
 				// iPod/iPhone
-				case (preg_match("/(ipod|iphone)/i", $this->rawUA) || $deviceOverride == "apple");
+				case (preg_match("/(ipod|iphone)/i", $this->rawUA) || ($deviceOverride == "apple"));
 					$this->name = "apple";
 					break;
 				// Android phones
-				case (preg_match("/android/i", $this->rawUA) || $deviceOverride == "android");
+				case (preg_match("/android/i", $this->rawUA) || ($deviceOverride == "android"));
 					$this->name = "android";
 					break;
 				// Opera mini
-				case (preg_match("/opera mini/i", $this->rawUA) || $deviceOverride == "opera");
+				case (preg_match("/opera mini/i", $this->rawUA) || ($deviceOverride == "opera"));
 					$this->name = "opera";
 					break;
 				// Blackberries
-				case (preg_match("/blackberry/i", $this->rawUA) || $deviceOverride == "blackberry");
+				case (preg_match("/blackberry/i", $this->rawUA) || ($deviceOverride == "blackberry"));
 					$this->name = "blackberry";
 					break;
 				// Palm phones
@@ -553,25 +553,25 @@
 					$this->name = "windows";
 					break;
 				// Other mobile devices
-				case (preg_match("/(mini 9.5|vx1000|lge |m800|e860|u940|ux840|compal|wireless| mobi|ahong|lg380|lgku|lgu900|lg210|lg47|lg920|lg840|lg370|sam-r|mg50|s55|g83|t66|vx400|mk99|d615|d763|el370|sl900|mp500|samu3|samu4|vx10|xda_|samu5|samu6|samu7|samu9|a615|b832|m881|s920|n210|s700|c-810|_h797|mob-x|sk16d|848b|mowser|s580|r800|471x|v120|rim8|c500foma:|160x|x160|480x|x640|t503|w839|i250|sprint|w398samr810|m5252|c7100|mt126|x225|s5330|s820|htil-g1|fly v71|s302|-x113|novarra|k610i|-three|8325rc|8352rc|sanyo|vx54|c888|nx250|n120|mtk |c5588|s710|t880|c5005|i;458x|p404i|s210|c5100|teleca|s940|c500|s590|foma|samsu|vx8|vx9|a1000|_mms|myx|a700|gu1100|bc831|e300|ems100|me701|me702m-three|sd588|s800|8325rc|ac831|mw200|brew |d88|htc\/|htc_touch|355x|m50|km100|d736|p-9521|telco|sl74|ktouch|m4u\/|me702|8325rc|kddi|phone|lg |sonyericsson|samsung|240x|x320vx10|nokia|sony cmd|motorola|up.browser|up.link|mmp|symbian|smartphone|midp|wap|vodafone|o2|pocket|kindle|mobile|psp|treo)/i", $this->rawUA));
+				case (preg_match("/(mini 9.5|vx1000|lge |m800|e860|u940|ux840|compal|wireless|ahong|lg380|lgku|lgu900|lg210|lg47|lg920|lg840|lg370|sam-r|mg50|s55|g83|t66|vx400|mk99|d615|d763|el370|sl900|mp500|samu3|samu4|vx10|xda_|samu5|samu6|samu7|samu9|a615|b832|m881|s920|n210|s700|c-810|_h797|mob-x|sk16d|848b|mowser|s580|r800|471x|v120|rim8|c500foma:|160x|x160|480x|x640|t503|w839|i250|sprint|w398samr810|m5252|c7100|mt126|x225|s5330|s820|htil-g1|fly v71|s302|-x113|novarra|k610i|-three|8325rc|8352rc|sanyo|vx54|c888|nx250|n120|mtk |c5588|s710|t880|c5005|i;458x|p404i|s210|c5100|teleca|s940|c500|s590|foma|samsu|vx8|vx9|a1000|_mms|myx|a700|gu1100|bc831|e300|ems100|me701|me702m-three|sd588|s800|8325rc|ac831|mw200|brew |d88|htc\/|htc_touch|355x|m50|km100|d736|p-9521|telco|sl74|ktouch|m4u\/|me702|8325rc|kddi|phone|lg |sonyericsson|samsung|240x|x320vx10|nokia|sony cmd|motorola|up.browser|up.link|mmp|symbian|smartphone|midp|wap|vodafone|o2|pocket|kindle|psp|treo)/i", $this->rawUA, $matches));
 					$this->name = "misc";
 					break;
 				// Other devices showing an accept for mobile content
 				case ((strpos($this->rawAccept,"text/vnd.wap.wml") > 0) || (strpos($this->rawAccept, "application/vnd.wap.xhtml+xml") > 0));
-					$this->name = "misc";
+					$this->name = "misc-wml";
 					break;
 				// Other devices sending a Profile header
-				case (isset($_SERVER["HTTP_X_WAP_PROFILE"]) || isset($_SERVER["HTTP_PROFILE"]));
-					$this->name = "misc";
+				case (!$deviceOverride && (isset($_SERVER["HTTP_X_WAP_PROFILE"]) || isset($_SERVER["HTTP_PROFILE"])));
+					$this->name = "misc-wap";
 					break;
 				// Final check against rare devices
-				case (in_array(strtolower(substr($this->rawUA, 0, 4)), $this->userAgents));
-					$this->name = "misc";
+				case (!$deviceOverride && in_array(strtolower(substr($this->rawUA, 0, 4)), $this->userAgents));
+					$this->name = "misc-rare";
 					break;
 				// Default behavior
-				case ($deviceOverride == "pc");
+				case ($deviceOverride == "desktop");
 				default;
-					$this->name = "pc";
+					$this->name = "desktop";
 					$this->mobile = false;
 					break;
 			}
